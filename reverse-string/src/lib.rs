5cc13e491c30187e565eeeb3a62703d11f8fd7db
Yukeien <antoine.duez@icloud.com>
2022-04-07 19:26:12 +0200

    [ReverseString] Turned project in

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,3 +1,23 @@
+extern crate unicode_segmentation;
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {}", input);
+    if input.len() <= 0 {
+        return String::from("");
+    }
+
+    let mut i = input.len() as i32 - 1;
+    let mut output = String::with_capacity(input.len());
+
+    while i >= 0 {
+        let char = input.graphemes(true).as_str().chars().nth(i as usize);
+
+        match char {
+            Some(char) => output.push(char),
+            None => ()
+        }
+        i -= 1;
+    }
+
+    output
 }
